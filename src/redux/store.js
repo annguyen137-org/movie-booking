19273373400dcde6c53b81c55ad8b5bfc4bb5569
annguyen13796 <nguyenlaman.1307@gmail.com>
2022-07-29 23:09:58 +0700

    done booking feature- not complete tickets result

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import movies from "./slices/moviesSlice";
 import theaters from "./slices/theatersSlice";
+import tickets from "./slices/ticketsSlice";
 
 const store = configureStore({
   reducer: {
     movies: movies,
     theaters: theaters,
     auth: authSlice,
+    tickets: tickets,
   },
 });
 
